ec690eccd0b0fedd674881e0c51fbdae9daf8c04
Kuddleman <edl1155@gmail.com>
2021-05-27 18:53:14 -0700

    add validation

--- src/section6oopPart1/Car.java
+++ src/section6oopPart1/Car.java
@@ -11,4 +11,18 @@ public class Car {
     private String engine;
     private String color;
 
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if(validModel.equals("carrera") || validModel.equals("commodore")){
+            this.model = model;
+        } else {
+            this.model = "unknown";
+        }
+
+    }
+
+    public String getModel() {
+        return this.model
+    }
+
 }
--- src/section6oopPart1/Main.java
+++ src/section6oopPart1/Main.java
@@ -4,4 +4,13 @@ public class Main {
     /* Classes have state and behavior
 
      */
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carrera");
+
+
+    }
+
 }
