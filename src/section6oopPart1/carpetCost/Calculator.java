987cff6f27d397c6c9ad1fd5234c8a709cf2f7d3
Kuddleman <edl1155@gmail.com>
2021-05-28 10:14:04 -0700

    complete carpet cost

---
+++ src/section6oopPart1/carpetCost/Calculator.java
@@ -0,0 +1,19 @@
+package section6oopPart1.carpetCost;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        double area = floor.getArea();
+        double cost = carpet.getCost();
+
+        return area * cost;
+
+    }
+}
