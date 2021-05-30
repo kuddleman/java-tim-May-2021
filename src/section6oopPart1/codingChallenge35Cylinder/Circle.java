fe253fb689a079bd1059e6f3eb3477470f068f31
Kuddleman <edl1155@gmail.com>
2021-05-29 17:38:27 -0700

    add Circle class

---
+++ src/section6oopPart1/codingChallenge35Cylinder/Circle.java
@@ -0,0 +1,23 @@
+package section6oopPart1.codingChallenge35Cylinder;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        if(radius < 0) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+}
