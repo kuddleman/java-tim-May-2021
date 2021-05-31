a076c4853e43784ce78aa29fb9c2be31b26eb07e
Kuddleman <edl1155@gmail.com>
2021-05-31 09:03:15 -0700

    add Printer class

--- src/section7oopPart2/codingChallengeCompositionPart2/encapsulation/Main.java
+++ src/section7oopPart2/codingChallengeCompositionPart2/encapsulation/Main.java
@@ -2,15 +2,21 @@ package section7oopPart2.codingChallengeCompositionPart2.encapsulation;
 
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player();
+//        Player player = new Player();
+//
+//        player.name = "Donny";
+//        player.health = 20;
+//        player.weapon = "sword";
+//
+//        int damage = 10;
+//        player.loseHeath(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
 
-        player.name = "Donny";
-        player.health = 20;
-        player.weapon = "sword";
+        EnhancedPlayer player = new EnhancedPlayer("Donny", 50, "Sword");
 
-        int damage = 10;
-        player.loseHeath(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
+
+
+
     }
 
 
