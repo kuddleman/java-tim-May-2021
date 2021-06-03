8d4bc5bf8f93f09f35b077ef3576022e919b3e00
Kuddleman <edl1155@gmail.com>
2021-06-02 19:58:03 -0700

    create Car class

---
+++ src/section7oopPart2/codingChallengeCompositionPart2/polymorphismChallenge/Car.java
@@ -0,0 +1,37 @@
+package section7oopPart2.codingChallengeCompositionPart2.polymorphismChallenge;
+
+public class Car {
+    private String name;
+    private boolean engine = true;
+    private int cylinders;
+    private int wheels = 4;
+
+    public Car(String name, boolean engine, int cylinders, int wheels) {
+        this.name = name;
+        this.engine = engine;
+        this.cylinders = cylinders;
+        this.wheels = wheels;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public boolean getEngine() {
+        return engine;
+    }
+    public int getCylinders() {
+        return cylinders;
+    }
+    public int getWheels() {
+        return wheels;
+    }
+    public void startEngine() {
+        System.out.println("CAR engine is starting!");
+    }
+    public void accelerate() {
+        System.out.println("CAR is accelerating!");
+    }
+    public void brake() {
+        System.out.println("CAR is braking!");
+    }
+}
