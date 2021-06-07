fe07689a93e525575130cfb9e0b8f741a500a7c7
Kuddleman <edl1155@gmail.com>
2021-06-07 07:55:54 -0700

    refactor remove method

---
+++ src/arraysListsBoxing/arrays/arrayList/Main.java
@@ -0,0 +1,13 @@
+package arraysListsBoxing.arrays.arrayList;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
+    public static void main(String[] args) {
+
+
+    }
+}
