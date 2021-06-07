c28736917a94abfd8a72cb43d1899a24dda6d44b
Kuddleman <edl1155@gmail.com>
2021-06-07 11:35:55 -0700

    add searchName method

--- src/arraysListsBoxing/arrays/arrayChallenge1/MobileTelephone.java
+++ src/arraysListsBoxing/arrays/arrayChallenge1/MobileTelephone.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class MobileTelephone {
+    private ArrayList<Contact> contactList = new ArrayList<>();
+
+    public void addContact(Contact contact) {
+        contactList.add(contact);
+    }
+    public ArrayList<Contact> getContactList(){
+        return contactList;
+    }
+    public void printContactList(){
+        System.out.println("You have " + contactList.size() +
+                " contacts in your contact list.");
+
+        for(int i = 0; i < contactList.size(); i++) {
+            System.out.println((i + 1) + ". " + contactList.get(i));
+        }
+    }
+
+    private int findItem(Contact contact){
+        String searchName = contact.getName();
+        for(int i = 0; i < contactList.size(); i++) {
+            String contactName = contactList.get(i).getName();
+            if(contactName.equalsIgnoreCase(searchName)) {
+               return i;
+            }
+        }
+        System.out.println("Contact is not in list.");
+        return -1;
+    }
 }
---
+++ src/arraysListsBoxing/arrays/arrayChallenge1/MobileTelephoneTester.java
@@ -0,0 +1,6 @@
+public class MobileTelephoneTester {
+
+    public static void main(String[] args) {
+
+    }
+}
