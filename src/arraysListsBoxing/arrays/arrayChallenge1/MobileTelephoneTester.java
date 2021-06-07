8afb83d31b0455fa6b5d38a4b90267a5a3b33fa9
Kuddleman <edl1155@gmail.com>
2021-06-07 12:06:16 -0700

    complete MobileTelephone methods

--- src/arraysListsBoxing/arrays/arrayChallenge1/MobileTelephoneTester.java
+++ src/arraysListsBoxing/arrays/arrayChallenge1/MobileTelephoneTester.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class MobileTelephoneTester {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        MobileTelephone mobile = new MobileTelephone();
+
+        Contact c1 = new Contact("Waif","4567");
+        Contact c2 = new Contact("Sleepy", "5643");
+        Contact c3 = new Contact("Crocker", "1254327");
 
     }
 }
