5d194a53b86000bd4ec9cc9fba381f473dd81336
Kuddleman <edl1155@gmail.com>
2021-06-12 16:38:48 -0700

    create song and album classes

---
+++ src/linkedLists/linkedListChallenge/Album.java
@@ -0,0 +1,4 @@
+package linkedLists.linkedListChallenge;
+
+public class Album {
+}
--- src/linkedLists/linkedListChallenge/Song.java
+++ src/linkedLists/linkedListChallenge/Song.java
@@ -1,4 +1,22 @@
 package linkedLists.linkedListChallenge;
 
 public class Song {
+    private String title;
+    private double duration;
+
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + this.title + "\n" +
+                "Duration: " + this.duration;
+    }
 }
