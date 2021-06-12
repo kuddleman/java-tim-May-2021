bd4b7e6da6b2510dcdb868d5104ee48a0d677f85
Kuddleman <edl1155@gmail.com>
2021-06-12 16:45:10 -0700

    add findSong method

--- src/linkedLists/linkedListChallenge/Album.java
+++ src/linkedLists/linkedListChallenge/Album.java
@@ -1,4 +1,30 @@
 package linkedLists.linkedListChallenge;
 
+import java.util.ArrayList;
+
 public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.songs = new ArrayList<Song>();
+    }
+    // use boolean for validation purposes
+    public boolean addSong(String title, double duration) {
+        if(findSong(title) == null) {
+            this.songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for(Song checkedSong : songs) {
+            if(checkedSong.getTitle().equals(title)){
+                return checkedSong;
+            }
+        }
+        return null;
+    }
 }
