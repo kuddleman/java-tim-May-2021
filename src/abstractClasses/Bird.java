b26f26dfce74af66da1c88171eac5842da9b504b
Kuddleman <edl1155@gmail.com>
2021-06-15 09:27:02 -0700

    add bird class

---
+++ src/abstractClasses/Bird.java
@@ -0,0 +1,17 @@
+package abstractClasses;
+
+public class Bird extends Animal {
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is pecking");
+    }
+
+    @Override
+    public void breathe() {
+        System.out.println("Birdie is breathing!");
+    }
+}
