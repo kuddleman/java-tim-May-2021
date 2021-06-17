f51f21140cf8c5d378fd7f93dbc2ff4b6d891654
Kuddleman <edl1155@gmail.com>
2021-06-17 08:39:05 -0700

    add Player class

--- src/abstractClasses/abstractClassChallenge/MyLinkedList.java
+++ src/abstractClasses/abstractClassChallenge/MyLinkedList.java
@@ -1,4 +1,30 @@
 package abstractClasses.abstractClassChallenge;
 
-public class MyLinkedList {
+public class MyLinkedList implements NodeList {
+
+    private ListItem root = null;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return null;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void transverse(ListItem root) {
+
+    }
 }
