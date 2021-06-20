704a66047f06da26c7abb2d6058744c968af1291
Kuddleman <edl1155@gmail.com>
2021-06-20 11:58:18 -0700

    rename product to productId

--- src/generics/GenericClassExample.java
+++ src/generics/GenericClassExample.java
@@ -1,5 +1,40 @@
 package generics;
 
 public class GenericClassExample {
+    Product product = new Product(121, "Soap");
+    int pId = product.g
 
+}
+class Product{
+    private Object productId;   //might be a string, integer...we don't know
+    private Object productDescription;
+
+    public Product(Object product, Object productDescription) {
+        this.productId = product;
+        this.productDescription = productDescription;
+    }
+
+    public void setProduct(Object productId) {
+        this.productId = productId;
+    }
+
+    public void setProductDescription(Object productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Object getProductId() {
+        return productId;
+    }
+
+    public Object getProductDescription() {
+        return productDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productDescription=" + productDescription +
+                '}';
+    }
 }
